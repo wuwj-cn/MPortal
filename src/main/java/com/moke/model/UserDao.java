0fc92168fbecf4918c30c338b19ded3293dde1bd
wuwj-cn <wenjie.0617@gmail.com>
2016-07-04 13:34:52 +0800

    add src package

---
+++ src/main/java/com/moke/model/UserDao.java
@@ -0,0 +1,12 @@
+package com.moke.model;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.repository.CrudRepository;
+
+@Transactional
+public interface UserDao extends CrudRepository<User, Long> {
+
+  public User findByUsername(String username);
+  
+}
