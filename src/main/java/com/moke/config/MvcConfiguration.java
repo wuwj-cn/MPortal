2d60df955cc12aad889138e0d8b965faddb4261b
wuwj-cn <wenjie.0617@gmail.com>
2016-07-09 21:07:43 +0800

    add MvcConfiguration.java

---
+++ src/main/java/com/moke/config/MvcConfiguration.java
@@ -0,0 +1,23 @@
+package com.moke.config;
+
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+//@Configuration
+//@EnableWebMvc
+public class MvcConfiguration extends WebMvcConfigurerAdapter {
+//    @Bean
+    public ViewResolver getViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/");
+        resolver.setSuffix(".html");
+        return resolver;
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }    
+}
